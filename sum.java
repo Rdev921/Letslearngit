3ec2e990449a5e07ec83f00042788e189208a888
Rdev921 <rishabhd92@gmail.com>
2022-03-03 22:16:11 +0530

    modified in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String args[]){
         int a = 5;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println("The sum of a and b is:"+ (a+b));
     }
 }
